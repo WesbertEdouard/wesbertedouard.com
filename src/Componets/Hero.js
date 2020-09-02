c1df1e02ab682351230bf66659e3ad7a73c4bc6f
Wesbert J Edouard <wesbert@mail.usf.edu>
2020-09-02 16:17:16 -0400

    changes to app.css and added hero componet

--- src/Componets/Hero.js
+++ src/Componets/Hero.js
@@ -18,7 +18,7 @@ function Hero(props) {
                         {props.subTitle3 && <h3 className='display-4 font-weight-light'>{props.subTitle3}</h3>}
                         {props.text && <h3 className='lead font-weight-light'>{props.text}</h3>}
                         <div className='pt-10'>
-                            <button className='Button' id='btn-red'>My Projects </button>
+                            <button className='Button' id='btn-red'>My Projects</button>
                             <button className='Button' id='btn-green'>Contact Me</button>
                         </div>
                     </Col>
