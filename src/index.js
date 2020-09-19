6ca50514411f506b20c5b9bafb64383ce2a3bfe7
Wesbert Edouard <wesbert@mail.usf.edu>
2020-09-19 02:52:09 -0400

    netlify data added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts/Nunito-BlackItalic.ttf';
+import './fonts/Nunito-ExtraLightItalic.ttf';
+import axios from 'axios';
 ReactDOM.render(
   <React.StrictMode>
     <App />
